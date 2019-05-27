8e3da276c1067c21936d9c1dde726d03f4d8bf89
Phani Kumar Kondramutla <phani9613@gmail.com>
2019-05-27 15:32:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port,() => {
 
-    console.log(`Server running at port `+port);
+    console.log(`Server is running at port `+port);
 
 });
